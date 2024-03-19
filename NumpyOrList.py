7639d26596793f67fad509661d0899d6475f6807
akashrajput1x <akash7f@gmail.com>
2024-03-19 10:38:17 +0530

    h

--- NumpyOrList.py
+++ NumpyOrList.py
@@ -4,7 +4,6 @@ import random
 
 #no of iterations
 n = 10000000
-
 list_1 = []*n
 array_1 = np.ndarray(n, dtype=int)
 
@@ -15,11 +14,9 @@ for i in range(n):
 start_time = time.time()
 list_1.sort()
 end_time = time.time()
-
 print(f"Time taken by python list to sort {n} values : {end_time - start_time}")
 
 start_time = time.time()
 np.sort(array_1)
 end_time = time.time()
-
 print(f"Time taken by numpy array to sort {n} values : {end_time - start_time}")
